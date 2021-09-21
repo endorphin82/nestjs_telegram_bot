1b492f4aa325e213eb0b81f90341c446b7ba09db
endorphin82 <endorphin82@gmail.com>
2021-09-21 16:01:19 +0300

    ports 3000

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TelegrafModule } from 'nestjs-telegraf';
 
 @Module({
-  imports: [],
+  imports: [
+    TelegrafModule.forRoot({
+      token: process.env.TELEGRAM_BOT_TOKEN,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
