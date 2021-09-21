3cefcb2f5fc41039da6c3dee01b998f2a5f9b838
endorphin82 <endorphin82@gmail.com>
2021-09-21 17:48:03 +0300

    check api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { BotService } from './bot.service';
 
 @Controller()
 export class AppController {
